7248dc9efd85850e5962c667e60e21b1a77391f4
Nicolás Gutiérrez (ElLimitador) <80768621+NicoJGutierrez@users.noreply.github.com>
2023-10-19 19:04:50 -0300

    feat: 3000/users/show muestra usuarios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import koa from "koa";
 import KoaLogger from "koa-logger";
 import { koaBody } from "koa-body";
-import routes from "./routes.js";
+import router from "./routes.js";
 
 const app = new koa();
 
 app.use(KoaLogger());
 app.use(koaBody());
 
+// koa-router
+app.use(router.routes());
+
 app.use((ctx, next) => {
-    ctx.body = "Hola mamaguevo\nQue hace?\nMe mira o que hace?"
+    ctx.body = "Hola mamey\nQue hace?\nMe mira o que hace?"
 });
 
 app.listen(3000, () =>{
-    console.log("Iniciando la wea de app en el puerto 3000");
+    console.log("Iniciando la app en el puerto 3000");
 });
