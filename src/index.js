bb7817be451b351a1def0e993f7f19425e657667
Nicolás Gutiérrez (ElLimitador) <80768621+NicoJGutierrez@users.noreply.github.com>
2023-10-19 14:56:13 -0300

    feat: app se inicia en el puerto 3000

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello world")
+import koa from "koa";
+
+const app = new koa();
+
+app.listen(3000, () =>{
+    console.log("Iniciando la wea de app , porfavor espere... \nEstá en el puerto 3000");
+});
