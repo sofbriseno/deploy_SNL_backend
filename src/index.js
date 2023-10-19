0ba2a8e4857820a4579800b694bf791aff3698ef
Nicolás Gutiérrez (ElLimitador) <80768621+NicoJGutierrez@users.noreply.github.com>
2023-10-19 15:27:42 -0300

    feat: koalogger y koabody

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import koa from "koa";
+import KoaLogger from "koa-logger";
+import { koaBody } from "koa-body";
 
 const app = new koa();
+
+app.use(KoaLogger());
+app.use(koaBody());
 
 app.use((ctx, next) => {
     ctx.body = "Hola mamaguevo\nQue hace?\nMe mira o que hace?"
