71b15f1a5da3f748a04a21cd32c2ae19ae35d9ad
SOFIA BRISENO <sofia.briseno@uc.cl>
2023-10-26 15:00:25 -0300

    feat: Sequelize Finders implementado en users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,23 +2,31 @@ const Router = require('koa-router');
 
 const router = new Router();
 
-const users = [
-    {
-        "user": "Nico",
-        "pass": "Bananatech"
-    },
-    {
-        "user": "Laura",
-        "pass": "12345678"
-    },
-    {
-        "user": "Sofia",
-        "pass": "12345678"
+router.post("users.create","/",async(ctx)=>{
+    try{
+        const user = await ctx.orm.Usuario.create(ctx.request.body);
+        ctx.body = user;
+        ctx.status = 201;
+    } catch(error){
+        ctx.body = error;
+        ctx.status = 400;
     }
-]
+})
 
-router.get("users.show", "/show", async(ctx) =>{
-    ctx.body = users;
+router.get("users.list","/",async(ctx)=>{
+    try{
+        const users = await ctx.orm.Usuario.findAll();
+        // Si quisieramos mostrar el usuario por su id sería de la siguiente manera:
+        // 1. const user = await ctx.orm.Usuario.findByPk(ctx.params.id); -> Este busca a partir de la PrimaryKey
+        // 2. const user = await ctx.orm.Usuario.findOne({where:{id:ctx.params.id}}); -> Este busca por filtros/condiciones
+        // ctx.body = user;
+        // y en postman el URL a usar seria /users/5
+        ctx.body = users;
+        ctx.status = 200;
+    } catch(error){
+        ctx.body = error;
+        ctx.status = 400;
+    }
 })
 
 module.exports = router;
