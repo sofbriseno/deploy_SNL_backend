d4d336dce4940a80067c1b1659a066f0c29fad18
SOFIA BRISENO <sofia.briseno@uc.cl>
2023-12-04 00:17:41 -0300

    fix: seed carta

--- src/seeders/20231027025331-seed-cards.js
+++ src/seeders/20231027025331-seed-cards.js
@@ -8,10 +8,41 @@ module.exports = {
     },
     {
       // ID = 2
+      tipo: 'Avanzar',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      // ID = 3
+      tipo: 'Avanzar',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      // ID = 4
+      tipo: 'Avanzar',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      // ID = 5
+      tipo: 'Avanzar',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      // ID = 6
+      tipo: 'Retroceder',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      // ID = 7
       tipo: 'Retroceder',
       createdAt: new Date(),
       updatedAt: new Date(),
     }
+
   ]),
   down: (queryInterface) => queryInterface.bulkDelete('Cartas', null, {}),
 };
