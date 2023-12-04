6e20838e812750c23d54c0c33a351077372c3575
SOFIA BRISENO <sofia.briseno@uc.cl>
2023-12-04 00:26:17 -0300

    fix: seed retroceder

--- src/seeders/20231027025747-seed-backwards.js
+++ src/seeders/20231027025747-seed-backwards.js
@@ -1,9 +1,16 @@
 module.exports = {
   up: (queryInterface) => queryInterface.bulkInsert('Retroceders', [
     {
-      id_carta: 2,
+      id_carta: 6,
       redireccion: 2,
       descripcion: 'Esta tarjeta te obliga a retroceder 2 casillas en el tablero. ¡Ten cuidado, no te quedes atrás!',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id_carta: 7,
+      redireccion: 1,
+      descripcion: '¡Retorno Inesperado! Esta tarjeta te obliga a retroceder una casilla en el tablero. ¡Vuelve un paso atrás, pero mantén tu determinación!',
       createdAt: new Date(),
       updatedAt: new Date(),
     }
